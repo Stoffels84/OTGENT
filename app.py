0b268f9ec80ebc050df0062f010d6af7add4c2ee
Stoffels84 <info@solutionsforyou.be>
2026-01-24 22:28:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -893,7 +893,7 @@ df_coach_voltooid_view = (
 # Pages
 # ----------------------------
 if current_page == "dashboard":
-    st.subheader("Dashboard (met update om 13u en 1u)")
+    st.subheader("Dashboard (met update om 1u en 13u)")
 
     q = st.text_input(
         "Zoek op personeelsnr of naam.",
